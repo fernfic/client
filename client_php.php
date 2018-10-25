3e76239ef167de9be3bab508ae86911a93669fef
fernfic <a5820097@gmail.com>
2018-10-25 15:30:58 +0700

    test2

--- client_php.php
+++ client_php.php
@@ -1,12 +1,42 @@
 <?php
 require_once('lib/nusoap.php');
 
+// $client = new nusoap_client("https://testselect-wolfbit.c9users.io/server.php?wsdl");
+
 $client = new nusoap_client("https://testselect.herokuapp.com/server.php?wsdl");
 
-// $client = new nusoap_client("https://testselect.herokuapp.com/server.php?wsdl");
-
 $data = array('room' => '01', 'time' => '12-09-2016 05:00', 'temp' => 22.5, 'humidity' => 10.12);
-$result = $client->call("get_data", array("room" => "01"));
-$result = $client->call("get_user", array("name" => "fern"));
+$kerry1 = array('name' => 'fern', 'addr' => '123', 'weight' => '22.5');
+$kerry2 = array('name' => 'pee', 'addr' => '345', 'weight' => '50');
+// $result = $client->call("send_kerry", array("data" => $kerry2));
+// $result = $client->call("update_kerry", array("data" => $kerry2));
+$result = $client->call("get_kerry", array("name" => "fern"));
 print_r($result);
+
+//function defination to convert array to xml
+function array_to_xml($array, &$xml_user_info) {
+    foreach($array as $key => $value) {
+        if(is_array($value)) {
+            if(!is_numeric($key)){
+                $subnode = $xml_user_info->addChild("$key");
+                array_to_xml($value, $subnode);
+            }else{
+                $subnode = $xml_user_info->addChild("item$key");
+                array_to_xml($value, $subnode);
+            }
+        }else {
+            $xml_user_info->addChild("$key",htmlspecialchars("$value"));
+        }
+    }
+}
+
+//creating object of SimpleXMLElement
+$xml_user_info = new SimpleXMLElement("<?xml version=\"1.0\"?><data></data>");
+
+//function call to convert array to xml
+array_to_xml($result,$xml_user_info);
+
+//saving generated xml file
+$xml_file = $xml_user_info->asXML('kerryData.xml');
+
 ?>
--- client_python.py
+++ client_python.py
@@ -2,13 +2,19 @@ from zeep import Client
 from dicttoxml import dicttoxml
 
 wsdl = "https://testselect.herokuapp.com/server.php?wsdl"
-
+# wsdl = "https://testselect-wolfbit.c9users.io/server.php?wsdl"
 soap_client = Client(wsdl)
 a = {'room' : '05', 'time' : '12-09-2016 05:00', 'temp' : '22.5', 'humidity' : '10.12'}
+kerry1 = {'name' : 'fern', 'addr' : '123', 'weight' : '22.5'}
+kerry2 = {'name' : 'pee', 'addr' : '345', 'weight' : '50'}
 
-test = soap_client.service.get_user("01")
+# test = soap_client.service.get_user('01')
 # test = soap_client.service.set_data(a)
 
+# test = soap_client.service.send_kerry(kerry)
+# test = soap_client.service.update_kerry(kerry)
+test = soap_client.service.get_kerry('1')
 print(test)
 
-# xml = dicttoxml(test, custom_root='air_data', attr_type=False)
+# xml = dicttoxml(test, custom_root='user_data', attr_type=False)
+# print(xml)
